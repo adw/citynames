49e724dfa47c0349d8b85f34fd34f5ea22cbba20
Andrew Walkingshaw <andrew@scrtplns.com>
2008-12-22 03:36:06 +0000

    Extra tweakables.

--- compare.py
+++ compare.py
@@ -30,9 +30,11 @@ def main(city):
     res = svg()
     cities = compare(city)
     multiple = 2.5
+    opacityscale = 2
     xmin = "0"; xmax = str(multiple*360)
     ymin = "0"; ymax = str(multiple*180)
     r = 1
+    color = "#000"
     res.append(line(x1=xmin, x2=xmax, y1=ymin, y2=ymin, strokewidth="1", 
                     stroke="black"))
     res.append(line(x1=xmin, x2=xmax, y1=ymax, y2=ymax, strokewidth="1", 
@@ -43,9 +45,10 @@ def main(city):
                     stroke="black"))
     for c in cities:
         x, y = (c[3]+180) * multiple, (c[2]-90) * -multiple
-        opacity = (float(c[0])/len(city))**2
+        opacity = (float(c[0])/len(city)) ** opacityscale
         res.append(circle(cy=str(y), cx=str(x), r=str(r), 
-                   style="stroke-width: 0; fill: #000; opacity: %s" % opacity, 
+                   style="stroke-width: 0; fill: %s; opacity: %s" % 
+                                                        (color, opacity),
                    id=unicode(c[1]+str(x)+str(y), encoding="utf-8")))
     tree = etree.ElementTree(res)
     tree.write(open(city+".svg", "wb"))
