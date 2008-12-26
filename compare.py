b4a32ac6d219704c9daa528b8b5ba8511ed79291
Andrew Walkingshaw <andrew@scrtplns.com>
2008-12-26 00:14:53 +0000

    As on LJ.

--- compare.py
+++ compare.py
@@ -11,30 +11,33 @@ def svgfactory(elem, **kwargs):
 svg = svgfactory("svg")
 circle = svgfactory("circle")
 line = svgfactory("line")
-
+text = svgfactory("text")
 def parse_table(data):
     return [line.split("\t") for line in data]
     
-def compare(city):
+def compare(city, limit=1):
     res = []
     city = city.lower()
     cities = parse_table(open("cities1000.txt"))
-    for c in cities:
-        comp = Comparator(city, c[2].lower())
+    for i in cities:
+        comp = Comparator(city, i[1].lower())
         subseq = comp.longest_substring()
-        if len(subseq) > 1:
-            res.append((len(subseq), c[2], float(c[4]), float(c[5])))
+        if len(subseq) > limit:
+            res.append((len(subseq), i[2], float(i[4]), float(i[5])))
     return sorted(res, reverse=True)
 
 def main(city):
     res = svg()
-    cities = compare(city)
+    limit = 1
     multiple = 2.5
     opacityscale = 2
+    opacityconstant = 0.5
     xmin = "0"; xmax = str(multiple*360)
     ymin = "0"; ymax = str(multiple*180)
-    r = 1
+    cities = compare(city, limit=3)
+    r = 2
     color = "#000"
+    hilite = "#f00"
     res.append(line(x1=xmin, x2=xmax, y1=ymin, y2=ymin, strokewidth="1", 
                     stroke="black"))
     res.append(line(x1=xmin, x2=xmax, y1=ymax, y2=ymax, strokewidth="1", 
@@ -43,13 +46,26 @@ def main(city):
                     stroke="black"))
     res.append(line(x1=xmin, x2=xmin, y1=ymin, y2=ymax, strokewidth="1", 
                     stroke="black"))
-    for c in cities:
-        x, y = (c[3]+180) * multiple, (c[2]-90) * -multiple
-        opacity = (float(c[0])/len(city)) ** opacityscale
-        res.append(circle(cy=str(y), cx=str(x), r=str(r), 
-                   style="stroke-width: 0; fill: %s; opacity: %s" % 
-                                                        (color, opacity),
-                   id=unicode(c[1]+str(x)+str(y), encoding="utf-8")))
+    t = text(x=str((multiple-0.25)*360), y=str((multiple-0.25)*180),
+             style="font-family: Helvetica;")
+    t.text = city
+    res.append(t)
+    for i in cities:
+        x, y = (i[3]+180) * multiple, (i[2]-90) * -multiple
+        scale = float(i[0])/len(city)
+        oscale = scale ** opacityscale
+        opacity = opacityconstant * oscale
+        if oscale == 1:
+            res.append(circle(cy=str(y), cx=str(x), r=str(r), 
+                       style="stroke-width: 0; fill: %s; opacity: %s" % 
+                                                            (hilite, 1),
+                       id=unicode(i[1]+str(x)+str(y), encoding="utf-8")))            
+        else:
+            res.append(circle(cy=str(y), cx=str(x), r=str(r), 
+                       style="stroke-width: 0.5; fill: white; stroke: %s; \
+                              opacity: %s" % (color, opacity),
+                       id=unicode(i[1]+str(x)+str(y), encoding="utf-8")))
+            
     tree = etree.ElementTree(res)
     tree.write(open(city+".svg", "wb"))
     
--- lnypm.py
+++ lnypm.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 import sys
 from lxml import etree
 from functools import partial
@@ -11,6 +13,7 @@ def svgfactory(elem, **kwargs):
 svg = svgfactory("svg")
 circle = svgfactory("circle")
 line = svgfactory("line")
+text = svgfactory("text")
 
 def parse_table(data):
     return [line.split("\t") for line in data]
@@ -26,11 +29,13 @@ def compare(city):
             res.append((len(subseq), c[2], float(c[4]), float(c[5])))
     return sorted(res, reverse=True)
 
-def main(city, startx=0, starty=0, color="#000", res=None, multiple=2.5, r=1):
+def main(city, startx=0, starty=0, color="#000", res=None, multiple=2.5, r=2,
+         hilite="#f00"):
     if res is None:
-        res = svg()
-    cities = compare(city)
+        res = svg(width="720", height="360", style="background-color: white;")
+    cities = compare(city.encode("latin-1"))
     opacityscale = 3
+    opacityconstant = 0.5
     xmin = str(startx); xmax = str(multiple*360 + startx)
     ymin = str(starty); ymax = str(multiple*180 + starty)
     res.append(line(x1=xmin, x2=xmax, y1=ymin, y2=ymin, strokewidth="1", 
@@ -41,20 +46,41 @@ def main(city, startx=0, starty=0, color="#000", res=None, multiple=2.5, r=1):
                     stroke="black"))
     res.append(line(x1=xmin, x2=xmin, y1=ymin, y2=ymax, strokewidth="1", 
                     stroke="black"))
-    for c in cities:
-        x, y = (c[3]+180) * multiple + startx, (c[2]-90) * -multiple + starty
-        opacity = (float(c[0])/len(city)) ** opacityscale
+
+    t = text(x=str((multiple-0.25)*360 + startx), 
+             y=str((multiple-0.25)*180 + starty),
+             style="font-family: Helvetica;")
+    t.text = city
+    defer = []
+    res.append(t)
+    for i in cities:
+        x, y = (i[3]+180) * multiple + startx, (i[2]-90) * -multiple + starty
+        scale = float(i[0])/len(city)
+        oscale = scale ** opacityscale
+        opacity = opacityconstant * oscale
+        if oscale == 1:
+            defer.append(i)
+        else:
+            res.append(circle(cy=str(y), cx=str(x), r=str(r), 
+                       style="stroke-width: 0.5; fill: none; stroke: %s; \
+                              opacity: %s" % (color, opacity),
+                       id=unicode(i[1]+str(x)+str(y), encoding="utf-8")))
+    for i in defer:
+        x, y = (i[3]+180) * multiple + startx, (i[2]-90) * -multiple + starty
+        scale = float(i[0])/len(city)
+        oscale = scale ** opacityscale
+        opacity = opacityconstant * oscale
         res.append(circle(cy=str(y), cx=str(x), r=str(r), 
                    style="stroke-width: 0; fill: %s; opacity: %s" % 
-                                                        (color, opacity),
-                   id=unicode(c[1]+str(x)+str(y), encoding="utf-8")))
+                                                        (hilite, 1),
+                   id=unicode(i[1]+str(x)+str(y), encoding="utf-8")))
     return res
     
 if __name__ == "__main__":
-    res = main("London", multiple=1, r=float(1)/2.5)
-    res = main("New York", multiple=1, r=float(1)/2.5, startx=360, res=res)
-    res = main("Paris", multiple=1, r=float(1)/2.5, starty=180, res=res)
-    res = main("M\314\210nchen", multiple=1, r=float(1)/2.5, starty=180, startx=360, res=res)
+    res = main("London", multiple=1, r=1)
+    res = main("New York", multiple=1, r=1, startx=360, res=res)
+    res = main("Paris", multiple=1, r=1, starty=180, res=res)
+    res = main(u"München", multiple=1, r=1, starty=180, startx=360, res=res)
     tree = etree.ElementTree(res)
     tree.write(open(sys.argv[1], "wb"))
     
